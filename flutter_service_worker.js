ad49bcb2c78ebc4f918c52b715c93cab1cb4fdc3
jindolma <jindolma@users.noreply.github.com>
2024-10-11 08:45:59 +0000

    deploy: 0522e35b967c61fbdfbed3c431421d046118fe16

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"index.html": "48dc85011134897c76b1504bb7e56ddd",
-"/": "48dc85011134897c76b1504bb7e56ddd",
+"index.html": "42f2902b5d2bc74961ba04c4637c77a2",
+"/": "42f2902b5d2bc74961ba04c4637c77a2",
 "manifest.json": "da04ad6cca4ff07d72911df0fa32fa6e",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -32,7 +32,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "6d91760d048e9465fcea625c5cd69610",
-"flutter_bootstrap.js": "a4aca7bb81e0b86fe9cd0916a3223208"};
+"flutter_bootstrap.js": "7f5f4785708e251b0bf68ae92c6c3eb7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
